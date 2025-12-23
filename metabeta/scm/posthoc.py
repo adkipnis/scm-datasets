0f6326df6a1278ef5ae07033249647d8de2635eb
adkipnis <adkipnis@mailbox.org>
2025-12-23 19:10:15 +0100

    base class

--- metabeta/scm/posthoc.py
+++ metabeta/scm/posthoc.py
@@ -1,9 +1,37 @@
 import torch
 from torch import nn
+from torch.nn import functional as F
 from torch import distributions as D
+from metabeta.scm.meta import Standardizer
 
-# --- probabilistic activations
-class Bernoulli(nn.Module):
+
+# --- deterministic post-hoc layers
+
+class Base(nn.Module):
+    def __init__(self,
+                 n_in: int,
+                 n_out: int,
+                 standardize: bool = False):
+        super().__init__()
+        self.n_in = n_in
+        self.n_out = n_out
+        self.standardize = standardize
+        if standardize:
+            self.standardizer = Standardizer()
+        alpha = torch.ones(n_in)
+        self.w = D.Dirichlet(alpha).sample((n_out, self.n_param)).permute(2,0,1)
+
+    @property
+    def n_param(self) -> int:
+        return 1
+
+    def preprocess(self, x: torch.Tensor) -> torch.Tensor:
+        if self.standardize:
+            x = self.standardizer(x)
+        x = torch.einsum('bnd,dap->bnap', x, self.w)
+        return x
+
+
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return torch.bernoulli(torch.sigmoid(x))
 
